9dcfa20d188a7f974a9b50080248947da8ef0819
Jason Green <jgreen44@asu.edu>
2022-05-02 23:48:01 -0500

    updated tests for gray color

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -21,3 +21,64 @@ test('button has correct initial color', () => {
     // expect the button text to be 'Change to red'
     expect(colorButton.textContent).toBe('Change to red')
 });
+
+test('initial conditions', () => {
+    render(<App/>);
+
+    // check that the button starts out enabled
+    const colorButton = screen.getByRole('button', {name: /change to blue/i});
+    expect(colorButton).toBeEnabled();
+
+    // check that the checkbox starts out unchecked
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).not.toBeChecked();
+
+})
+
+test('checkbox functionality', () => {
+    render(<App />)
+
+    // get button
+    const colorButton = screen.getByRole('button', {name: /change to blue/i})
+
+    // checkbox checked,
+    const checkbox = screen.getByRole('checkbox', {name: 'Disable button'});
+    fireEvent.click(checkbox)
+
+    // expect checkbox to be now checked
+    expect(checkbox).toBeChecked()
+
+    // expect button to be disabled
+    expect(colorButton).toBeDisabled()
+
+    // checkbox unchecked
+    fireEvent.click(checkbox);
+
+    // expect checkbox to be unchecked
+    expect(checkbox).not.toBeChecked()
+
+    // expect button to be enabled
+    expect(colorButton).toBeEnabled()
+})
+
+test('button is gray when disabled', () => {
+    render(<App />);
+
+    // flow:
+    // get button and checkbox
+    const colorButton = screen.getByRole('button', {name: /change to blue/i})
+    const checkbox = screen.getByRole('checkbox', {name: /disable button/i})
+
+    // disable button by clicking checkbox
+    fireEvent.click(checkbox);
+
+    // button is gray
+    expect(colorButton).toHaveStyle({backgroundColor: 'gray'})
+
+    // enabled button by clicking on checkbox again
+    fireEvent.click(checkbox);
+
+    // button is red
+    expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+
+})
--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,32 @@ import React from 'react';
 import './App.css';
 import {useState} from "react";
 
+
+
 function App() {
     const [buttonColor, setButtonColor] = useState('red');
+    const [disabled, setDisabled] = useState(false)
     const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
+    const handleButtonState = () => {
+        setDisabled(prevState => !prevState)
+    }
+
+
 
   return (
     <div >
       <button
-          style={{backgroundColor: buttonColor}}
+          style={{backgroundColor: disabled ? 'gray': buttonColor}}
+          disabled={disabled}
           onClick={() => setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
+        <input 
+            type={'checkbox'} 
+            aria-checked={disabled}
+            defaultChecked={disabled}
+            id={'disable-button-checkbox'}
+            onClick={handleButtonState}/>
+        <label htmlFor={'disable-button-checkbox'}>Disable button</label>
     </div>
   );
 }
