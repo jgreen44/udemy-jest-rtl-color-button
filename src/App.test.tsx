f22ac39988cd6d4115159ad346939079f412f3fe
Jason Green <jgreen44@asu.edu>
2022-05-02 22:48:57 -0500

    updated tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import App from "./App";
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 
 
 test('button has correct initial color', () => {
     render(<App/>);
 
     // find an element with a role of button and text of 'Change to blue'
-    const colorButton = screen.getByRole('button',{ name: /change to blue/i});
+    const colorButton = screen.getByRole('button', {name: /change to blue/i});
 
     // expect the background color to be red
     expect(colorButton).toHaveStyle({backgroundColor: 'red'})
-});
 
+    // click button
+    fireEvent.click(colorButton);
 
-test('button turns blue when clicked', () => {
+    // expect to have background color of blue
+    expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
 
+    // expect the button text to be 'Change to red'
+    expect(colorButton.textContent).toBe('Change to red')
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [buttonColor, setButtonColor] = useState('red');
+    const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
     <div >
-      <button style={{backgroundColor: 'red'}}>Change to blue</button>
+      <button
+          style={{backgroundColor: buttonColor}}
+          onClick={() => setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
     </div>
   );
 }
