4dea2b6337e8a5a5b18e1e319dcd87bebf743e25
Jason Green <jgreen44@asu.edu>
2022-05-03 00:51:08 -0500

    prettier applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 export const replacePascalCaseWithSpace = (colorName: string) => {
-
-    return colorName.replace(/\B([A-Z])\B/g, ' $1');
-}
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+};
 
 function App() {
-    const [buttonColor, setButtonColor] = useState('MediumVioletRed');
-    const [disabled, setDisabled] = useState(false)
-    const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
+  const [disabled, setDisabled] = useState(false);
+  const newButtonColor =
+    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
-    const handleButtonState = () => {
-        setDisabled(prevState => !prevState)
-    }
+  const handleButtonState = () => {
+    setDisabled((prevState) => !prevState);
+  };
 
-
-    return (
-        <div>
-            <button
-                style={{backgroundColor: disabled ? 'gray' : buttonColor}}
-                disabled={disabled}
-                onClick={() => setButtonColor(newButtonColor)}>Change to {newButtonColor}</button>
-            <input
-                type={'checkbox'}
-                aria-checked={disabled}
-                defaultChecked={disabled}
-                id={'disable-button-checkbox'}
-                onClick={handleButtonState}/>
-            <label htmlFor={'disable-button-checkbox'}>Disable button</label>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        disabled={disabled}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+      <input
+        type={"checkbox"}
+        aria-checked={disabled}
+        defaultChecked={disabled}
+        id={"disable-button-checkbox"}
+        onClick={handleButtonState}
+      />
+      <label htmlFor={"disable-button-checkbox"}>Disable button</label>
+    </div>
+  );
 }
 
 export default App;
